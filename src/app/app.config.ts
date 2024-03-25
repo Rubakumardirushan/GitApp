5f59dd98350ed0dcc0fea34858d50d06f770dae6
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-03-25 22:06:13 +0530

    add

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { withFetch ,provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideHttpClient()]
+
 };
